86226c01f9ceae2da858b2ca0e9a68ed2b5e0b66
Andrew Carrigan <carrigan223@gmail.com>
2020-10-26 12:03:55 -0700

    Intro to Gatsby part 1 implementing styles and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <Layout>
+    <h1>Home</h1>
+    <p>Hello California</p>
+    <Link to="/about/">&rarr; About</Link>
+  </Layout>
+);
