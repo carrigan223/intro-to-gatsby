1bfd8361a52a1f5434377eb6997f373254898dae
Andrew Carrigan <carrigan223@gmail.com>
2020-10-27 10:45:34 -0700

    added Posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,25 @@
 module.exports = {
-    plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Andrews Gatsby first site',
+    description: 'My Front end Masters Gatsby workshop site',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello California</p>
-    <Link to="/about/">&rarr; About</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello California</p>
+      <Link to="/about/">&rarr; About</Link>
+
+      <h2>Read My Blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
