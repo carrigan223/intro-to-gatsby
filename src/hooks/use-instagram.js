8dfd53fd2421fa47a040475762765127cce6c733
Andrew Carrigan <carrigan223@gmail.com>
2020-11-03 01:29:24 -0800

    completed gatsby course site

---
+++ src/hooks/use-instagram.js
@@ -0,0 +1,30 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useInstagram = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allInstaNode(limit: 12) {
+        nodes {
+          id
+          caption
+          username
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 120, maxHeight: 120) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return data.allInstaNode.nodes.map(node => ({
+    ...node.localFile.childImageSharp,
+    id: node.id,
+    caption: node.caption,
+    username: node.username === '8385666633' ? 'gatsbyjs' : node.username,
+  }));
+};
+export default useInstagram;
